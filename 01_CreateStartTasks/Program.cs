1a11c828b664f9a032b7bcbe4e88f4c18886eaa0
simutaroman <simutaroman@gmail.com>
2018-11-14 19:35:32 +0200

    Add project files.

---
+++ 01_CreateStartTasks/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_CreateStartTasks
+{
+    class Program
+    {
+        public static void Write(char c)
+        {
+            int i = 1000;
+            while (i-- > 0)
+            {
+                Console.Write(c);
+            }
+        }
+
+        public static void Write(object o)
+        {
+            int i = 1000;
+            while (i-- > 0)
+            {
+                Console.Write(o);
+            }
+        }
+
+        public static int TextLenth(object o)
+        {
+            Console.WriteLine($"\nTask with id={Task.CurrentId} is processing object {o}");
+            return o.ToString().Length;
+        }
+
+        static void Main(string[] args)
+        {
+            //Task.Factory.StartNew(() => Write('.'));
+            //var t = new Task(() => Write('?'));
+            //t.Start();
+            //Write('!');
+
+            //Task t2 = new Task(Write, "Hello");
+            //t2.Start();
+            //Task.Factory.StartNew(Write, 1234);
+
+            string text1 = "testing", text2 = "this";
+            var task1 = new Task<int>(TextLenth, text1);
+            task1.Start();
+
+            Task<int>task2 = Task.Factory.StartNew(TextLenth, text2);
+
+            Console.WriteLine($"The length of the {text1} is {task1.Result}");
+            Console.WriteLine($"The length of the {text2} is {task2.Result}");
+
+            Console.WriteLine("Main thread done");
+            Console.ReadKey();
+        }
+
+        
+    }
+}
+
---
+++ CancelTask/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CancelTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var cts = new CancellationTokenSource();
+            //var token = cts.Token;
+            //token.Register(() =>
+            //{
+            //    Console.WriteLine("Cancellation has been requested");
+            //});
+            //var t = new Task(() =>
+            //{
+            //    var i = 0;
+            //    while (true)
+            //    {
+            //        //canonical exit 2 (3)
+            //        token.ThrowIfCancellationRequested();
+            //        //if (token.IsCancellationRequested)
+            //        //{
+            //        //    //release resources, if any
+            //        //    //break;//soft exit (1)
+            //        //    throw new OperationCanceledException();//canonical exit 1. exception is not propagated (2)
+            //        //}
+
+            //        Console.Write($"{i++}\t");
+            //    }
+            //}, token);
+            //t.Start();
+
+            //Task.Factory.StartNew(() =>{
+            //    token.WaitHandle.WaitOne();
+            //    Console.WriteLine("Wait handle released. Cancellation was requested");
+            //});
+
+            //Console.ReadKey();
+            //cts.Cancel();
+
+
+            var planned = new CancellationTokenSource();
+            var preventative = new CancellationTokenSource();
+            var emergency = new CancellationTokenSource();
+
+            planned.Token.Register(() =>
+            {
+                Console.WriteLine("planned cancellation..."); 
+            });
+            preventative.Token.Register(() =>
+            {
+                Console.WriteLine("preventative cancellation...");
+            });
+            emergency.Token.Register(() =>
+            {
+                Console.WriteLine("emergency cancellation...");
+            });
+
+            var paranoid = CancellationTokenSource.CreateLinkedTokenSource(
+                planned.Token, preventative.Token, emergency.Token);
+
+            Task.Factory.StartNew(() =>
+            {
+                var i = 0;
+                while (true)
+                {
+                    paranoid.Token.ThrowIfCancellationRequested();
+                    Console.Write($"{i++}\t");
+                    Thread.Sleep(1000);
+                }
+            }, paranoid.Token);
+
+            Console.ReadKey();
+            // any of tokens works
+            planned.Cancel();
+            //emergency.Cancel();
+            //preventative.Cancel();
+
+            Console.WriteLine("Main thread done");
+            //Console.ReadKey();
+        }
+    }
+}
