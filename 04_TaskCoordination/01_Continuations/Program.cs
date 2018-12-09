eaae745947fcfe990289893da0227439f0c09519
simutaroman <simutaroman@gmail.com>
2018-12-09 21:29:03 +0200

    task coordinations

---
+++ 04_TaskCoordination/01_Continuations/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Continuations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var task = Task.Factory.StartNew(() => { Console.WriteLine("Boling water"); });
+            //var task2 = task.ContinueWith(t => { Console.WriteLine($"Completed task {t.Id}, pour water into cup."); });
+            //task2.Wait();
+
+            var task = Task.Factory.StartNew(() => "Task1");
+            var task2 = Task.Factory.StartNew(() => "Task2");
+
+            //var task3 = Task.Factory.ContinueWhenAll(new[] {task, task2},
+            //    tasks =>
+            //    {
+            //        Console.WriteLine("Task completed:");
+            //        foreach (var t in tasks)
+            //        {
+            //            Console.WriteLine(" - " + t.Result);
+            //        }
+
+            //        Console.WriteLine("All tasks done");
+            //    });
+
+            var task3 = Task.Factory.ContinueWhenAny(new[] { task, task2 },
+                t =>
+                {
+                    Console.WriteLine("Task completed:");
+                    Console.WriteLine(" - " + t.Result);
+                    Console.WriteLine("All tasks done");
+                });
+            task3.Wait();
+        }
+    }
+}
---
+++ 04_TaskCoordination/02_ChildTask/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _02_ChildTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parent = new Task(() =>
+            {
+                //detached
+                var child = new Task(()=>
+                {
+                    Console.WriteLine("Child task starting;");
+                    Thread.Sleep(3000);
+                    Console.WriteLine("Child task finishing");
+                    throw new Exception();
+                }, TaskCreationOptions.AttachedToParent);
+                child.Start();
+                var completionHandler =
+                    child.ContinueWith(t => { Console.WriteLine($"Hooray, task {t.Id}'s state is {t.Status}"); },
+                        TaskContinuationOptions.AttachedToParent | TaskContinuationOptions.OnlyOnRanToCompletion);
+                var failHandler =
+                    child.ContinueWith(t => { Console.WriteLine($"Oops, task {t.Id}'s state is {t.Status}"); },
+                        TaskContinuationOptions.AttachedToParent | TaskContinuationOptions.OnlyOnFaulted);
+
+            });
+            
+
+            parent.Start();
+            try
+            {
+                parent.Wait();
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle((e=>true));
+                //throw;
+            }
+        }
+    }
+}
