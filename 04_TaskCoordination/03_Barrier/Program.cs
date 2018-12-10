93afb2c799fb1c957a525a8f78b15ffaa9c03f01
Roman Simuta <simutaroman@gmail.com>
2018-12-10 17:55:37 +0200

    restructuring. barrier and countdown events

---
+++ 04_TaskCoordination/03_Barrier/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _03_Barrier
+{
+    class Program
+    {
+        static Barrier barrier = new Barrier(2, b=>
+        {
+            Console.WriteLine($"Phase {b.CurrentPhaseNumber} is finished");
+            
+        });
+
+        public static void Water()
+        {
+            Console.WriteLine("Putting the kettle on (takes a bit longer)");
+            Thread.Sleep(2000);
+            barrier.SignalAndWait();// _1_2
+            Console.WriteLine("Pouring the water into the cup");// _2_0
+            barrier.SignalAndWait();//_2_1
+            Console.WriteLine("Putting the kettle on away");
+
+        }
+
+        public static void Cup()
+        {
+            Console.WriteLine("Finding the nicest cup of tea (fast)");
+            barrier.SignalAndWait();// _1_1
+            Console.WriteLine("Adding tea");
+            barrier.SignalAndWait();//_2_2
+            Console.WriteLine("Adding sugar");
+
+        }
+        static void Main(string[] args)
+        {
+            var water = Task.Factory.StartNew(Water);
+            var cup = Task.Run(new Action(Cup));
+
+            var tea = Task.Factory.ContinueWhenAll(new[] { water, cup }, tasks =>
+             {
+                 Console.WriteLine("Enjoy your cup of tea");
+             });
+            tea.Wait();
+        }
+    }
+}
---
+++ 04_TaskCoordination/04_CountDownEvent/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _04_CountDownEvent
+{
+    class Program
+    {
+        private static int taskCount = 5;
+        static CountdownEvent cte = new CountdownEvent(taskCount);
+        static Random random = new Random();
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < taskCount; i++)
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine($"Entering task {Task.CurrentId}");
+                    Thread.Sleep(random.Next(3000));
+                    cte.Signal();
+                    Console.WriteLine($"Exinting task {Task.CurrentId}");
+                });
+            }
+
+            var final = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine($"Waiting for other tasks to complete in {Task.CurrentId}");
+                cte.Wait();
+                Console.WriteLine("All tasks completed");
+            });
+            final.Wait();
+        }
+    }
+}
