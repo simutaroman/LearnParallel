33ee5b5339616a7656f4f19584a5f46ac9f8b105
Roman Simuta <simutaroman@gmail.com>
2018-12-11 18:04:34 +0200

    parallel loop storage and partitioning

---
+++ 05_ParallelLoops/03_ThreadLocalStorage/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _03_ThreadLocalStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            //Parallel.For(1, 1001, x =>
+            //{
+            //    Interlocked.Add(ref sum, x);// too much of interlocked
+            //});
+
+            
+            // calculate partial sum in thread local storage
+            // then, calculate common sum
+            Parallel.For(1, 1001,
+                () => 0,
+                (x, state, tls) =>
+                {
+                    tls += x;
+                    Console.WriteLine($"Task {Task.CurrentId} has partial sum {tls}");
+                    return tls;
+                }, 
+                partialSum =>
+                {
+                    Console.WriteLine($"Partial value of task {Task.CurrentId} is {partialSum}");
+                    Interlocked.Add(ref sum, partialSum);
+                });
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
---
+++ 05_ParallelLoops/05_Partitioning/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace _05_Partitioning
+{
+    public class Program
+    {
+        [Benchmark]
+        public static void SquareEachValue()
+        {
+            const int count = 100000;
+            var values = Enumerable.Range(0, count);
+            var results = new int[count];
+            Parallel.ForEach(values, x => { results[x] = (int)Math.Pow(x, 2); });
+        }
+
+        [Benchmark]
+        public static void SquareEachValueChunked()
+        {
+            const int count = 100000;
+            var values = Enumerable.Range(0, count);
+            var results = new int[count];
+            var part = Partitioner.Create(0, count, 100000);
+            Parallel.ForEach(part, range =>
+            {
+                for(int i = range.Item1; i < range.Item2; i++)
+                {
+                    results[i] = (int)Math.Pow(i, 2);
+                }
+               
+            });
+        }
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Program>();
+            Console.WriteLine(summary);
+
+        }
+    }
+}
